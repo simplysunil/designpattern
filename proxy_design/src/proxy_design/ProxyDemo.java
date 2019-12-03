fc08bba6535b91c368715ba7c9116b083c441089
sunil <sunilit01@gmail.com>
2019-12-03 15:02:15 +0530

    Changes commited

--- proxy_design/src/proxy_design/ProxyDemo.java
+++ proxy_design/src/proxy_design/ProxyDemo.java
@@ -3,6 +3,7 @@ package proxy_design;
 public class ProxyDemo {
 
 	public static void main(String[] args) {
+		System.out.println("********** This is proxy demo ***********");
 		ProxyObject proxyObject = new ProxyObject();
 		proxyObject.provideUpdates();
 
